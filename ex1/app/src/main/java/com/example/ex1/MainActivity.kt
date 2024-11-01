ea76a45dc716bcb90a3a9a1140e64b03fed1dc8b
finnyjohnson <109125747+finnyjohnson@users.noreply.github.com>
2024-11-01 09:27:43 +0530

    exercise 1

---
+++ ex1/app/src/main/java/com/example/ex1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ex1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Button Clicked!"
+        }
+    }
+}
